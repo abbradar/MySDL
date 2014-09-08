5e23e2d353c01a1ae41b7be2aa90dad7321dd03a
Nikolay Amiantov <nikoamia@gmail.com>
2014-09-09 03:56:19 +0400

    Silly me, this is a Monoid -- no difference at all

--- src/FRP/Netwire/Extra.hs
+++ src/FRP/Netwire/Extra.hs
@@ -14,12 +14,7 @@ mapE f = mkSF_ $ \case
 emptyE :: Wire s e m (Event a) (Event ())
 emptyE = mapE $ const ()
 
-(<&&) :: Monad m => Wire s e m a (Event b) -> Wire s e m a (Event c) -> Wire s e m a (Event ())
-a <&& b = emptyE . a <& emptyE . b
+(<!>) :: Monad m => Wire s e m a (Event b) -> Wire s e m a (Event c) -> Wire s e m a (Event ())
+a <!> b = emptyE . a <> emptyE . b
 
-infixl 5 <&&
-
-(&&>) :: Monad m => Wire s e m a (Event b) -> Wire s e m a (Event c) -> Wire s e m a (Event ())
-a &&> b = emptyE . a &> emptyE . b
-
-infixl 5 &&>
+infixl 5 <!>
